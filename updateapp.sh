96b3f3de363ba2f79ecf2ee744fe39ed1c022b22
Markus Sadler <msadler@live.at>
2022-11-23 22:27:03 +0100

    delete updateapp.sh

--- updateapp.sh
+++
@@ -1,6 +0,0 @@
-git pull
-
-docker build -t wrlinientomqqtt:$1 .
-docker stop wrLininenToMqtt
-docker rm wrLininenToMqtt
-docker run -d --restart unless-stopped --name wrLininenToMqtt wrlinientomqqtt:$1
